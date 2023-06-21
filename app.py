3300e2ab5b00faf00626cd6e4447ef2981bc77da
tzuk999 <tzuk999@gmail.com>
2023-06-21 14:23:27 +0300

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect,jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///store.db'
 db = SQLAlchemy(app)
 
